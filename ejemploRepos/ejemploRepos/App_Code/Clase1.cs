543336ae318276c3e9f3d6d7f9039962d42b32ce
Camilo Andrés <cass465andres@gmail.com>
2020-03-05 20:43:31 -0500

    commit clase 1

---
+++ ejemploRepos/ejemploRepos/App_Code/Clase1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Clase1
+/// </summary>
+public class Clase1
+{
+    public Clase1()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
